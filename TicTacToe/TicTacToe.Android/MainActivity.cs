bd4bad4306cce6d7d92027811f3b1721ba08fb06
Matthew Leibowitz <mattleibow@live.com>
2019-03-30 02:24:47 +0200

    Fixed the whitespace in the platform code

--- TicTacToe/TicTacToe.Android/MainActivity.cs
+++ TicTacToe/TicTacToe.Android/MainActivity.cs
@@ -1,25 +1,23 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
 namespace TicTacToe.Droid
 {
-    [Activity(Label = "TicTacToe", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+	[Activity(Label = "TicTacToe", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
-        }
-    }
+			base.OnCreate(savedInstanceState);
+
+			Forms.Init(this, savedInstanceState);
+			LoadApplication(new App());
+		}
+	}
 }
