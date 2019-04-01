ef77e27599b6d8d31bf4030a81766c3f133ceb40
Matthew Leibowitz <mattleibow@live.com>
2019-04-01 22:41:03 +0200

    Added the first view model

---
+++ TicTacToe/TicTacToe/GameViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TicTacToe
+{
+	public enum GameState
+	{
+		InProgress,
+		GameOver
+	}
+
+	public enum Player
+	{
+		Nobody,
+		X,
+		O
+	}
+
+	public class GameViewModel : BindableObject
+	{
+		private Player currentPlayer;
+		private Player winner;
+		private GameState state;
+
+		public GameViewModel()
+		{
+			MakeMoveCommand = new Command(OnMakeMove);
+			Board = new Player[9];
+			CurrentPlayer = Player.X;
+		}
+
+		public ICommand MakeMoveCommand { get; }
+
+		public Player[] Board { get; }
+
+		public Player CurrentPlayer
+		{
+			get { return currentPlayer; }
+			set
+			{
+				currentPlayer = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public Player Winner
+		{
+			get { return winner; }
+			set
+			{
+				winner = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public GameState State
+		{
+			get { return state; }
+			set
+			{
+				state = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private void OnMakeMove()
+		{
+			CurrentPlayer = CurrentPlayer == Player.X
+				? Player.O
+				: Player.X;
+		}
+	}
+}
