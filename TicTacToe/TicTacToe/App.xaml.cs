ec670288839e239f9aa37a162d6af6ac5adb8879
Matthew Leibowitz <mattleibow@live.com>
2019-04-16 15:37:54 +0400

    Added initial analytics

--- TicTacToe/TicTacToe/App.xaml.cs
+++ TicTacToe/TicTacToe/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
@@ -17,6 +20,16 @@ namespace TicTacToe
 		protected override void OnStart()
 		{
 			// Handle when your app starts
+
+#if DEBUG
+			AppCenter.LogLevel = LogLevel.Verbose;
+#endif
+
+			AppCenter.Start(
+				"android=5400d30b-a163-4ae8-ac99-8a886ba60e82;" +
+				"ios=972a7d89-3b25-4d5f-88e5-302e0ba0d634;" +
+				"uwp=a498e3f2-448c-4b7d-95e6-9352800dd652;",
+				typeof(Analytics), typeof(Crashes));
 		}
 
 		protected override void OnSleep()
