1b2fd99b5ed8b01f2192cd9ae40cc2c13f0fea46
Matthew Leibowitz <mattleibow@live.com>
2019-04-16 13:53:52 +0400

    Added initial endgame detection

--- TicTacToe/TicTacToe/ViewModels/GameViewModel.cs
+++ TicTacToe/TicTacToe/ViewModels/GameViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using System.Linq;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace TicTacToe
@@ -66,9 +67,64 @@ namespace TicTacToe
 			Board[index] = CurrentPlayer;
 			OnPropertyChanged(nameof(Board));
 
-			CurrentPlayer = CurrentPlayer == Player.X
-				? Player.O
-				: Player.X;
+			var possible = DetectGameOver();
+			if (possible != null)
+			{
+				// there was a winner or a tie
+				Winner = possible.Value;
+				State = GameState.GameOver;
+				CurrentPlayer = Player.Nobody;
+			}
+			else
+			{
+				// we are still going
+				CurrentPlayer = CurrentPlayer == Player.X
+					? Player.O
+					: Player.X;
+			}
+		}
+
+		private Player? DetectGameOver()
+		{
+			// check vertical for winner
+			for (var i = 0; i < 3; i++)
+			{
+				var vertWin = CheckLine(i, i + 3, i + 6);
+				if (vertWin != Player.Nobody)
+					return vertWin;
+			}
+
+			// check horizontal for winner
+			for (var i = 0; i < 9; i += 3)
+			{
+				var horizWin = CheckLine(i, i + 1, i + 2);
+				if (horizWin != Player.Nobody)
+					return horizWin;
+			}
+
+			// check top-left to bottom-right for winner
+			var diag1Win = CheckLine(0, 4, 8);
+			if (diag1Win != Player.Nobody)
+				return diag1Win;
+
+			// check top-right to bottom-left for winner
+			var diag2Win = CheckLine(2, 4, 6);
+			if (diag2Win != Player.Nobody)
+				return diag2Win;
+
+			// check tie
+			if (Board.All(p => p != Player.Nobody))
+				return Player.Nobody;
+
+			return null;
+
+			Player CheckLine(int a, int b, int c)
+			{
+				if (Board[a] == Board[b] && Board[a] == Board[c])
+					return Board[a];
+
+				return Player.Nobody;
+			}
 		}
 	}
 }
