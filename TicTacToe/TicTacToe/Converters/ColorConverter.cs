be2d9e171aae16b690f04c622aa84b6de8d283b0
Matthew Leibowitz <mattleibow@live.com>
2019-04-16 14:23:07 +0400

    Added some color to the winning moves

---
+++ TicTacToe/TicTacToe/Converters/ColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TicTacToe
+{
+	public class ColorConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var player = (Player)value;
+
+			if (player.HasFlag(Player.IsWinner))
+				return App.Current.Resources["DefaultRed"];
+
+			return App.Current.Resources["AlmostBlack"];
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
